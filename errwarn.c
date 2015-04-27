9756610c5bad9f5500d4d6ccd454ae4eafcf70f1
root <root@localhost.localdomain>
2015-04-27 13:56:58 +0800

    修改errwarn.c文件，将error和warning日志信息导出到test.txt文件中

--- errwarn.c
+++ errwarn.c
@@ -1,6 +1,36 @@
 #include<stdio.h>
 #include<stdlib.h> 
-void main(){
-    system("grep -i error /var/log/messages");
-    system("grep -i warning /var/log/messages");
+#include<string.h>
+int loop()
+{
+    char str[256];
+    FILE *fp1,*fp2;
+    if ((fp1=fopen("/var/log/messages","r"))==NULL)
+    { /* 以只读方式打开文件1 */
+        printf("cannot open file\n");
+        exit(0);
+    }
+    if((fp2=fopen("/home/lookfiresu/four-level/test.txt","w"))==NULL)
+    { /*以只写方式打开文件2 */
+        printf("cannot open file\n");
+        exit(0);
+    }
+    while((fgets(str,256,fp1))!=NULL)
+    /*从文件中读回的字符串长度大于0 */
+    {
+	if(strstr(str,"error")||strstr(str,"ERROR")||strstr(str,"warning")||strstr(str,"WARNING"))
+        {
+            fputs(str,fp2 ); /* 从文件1读字符串并写入文件2 */
+        }
+    }
+    fclose(fp1);
+    fclose(fp2);
+}
+
+void main()
+{
+    while(1){
+	loop();
+	sleep(30);
+    }
 }
